6ac7da11309521074d3b70f639ec690d48bfb7e0
jbeltrami <joao_beltrami@hotmail.com>
2019-06-17 11:54:52 -0400

    Create a functional component to serve as a wrapper for CommentDetail component. Learn how to pass in Components as props to other Components

--- src/index.js
+++ src/index.js
@@ -2,27 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => (
   <div className="ui container comments">
-    <CommentDetail
-      author={faker.name.firstName()}
-      timeAgo="Today at 4:45PM"
-      avatar={faker.image.avatar()}
-      content={faker.random.words()}
-    />
-    <CommentDetail
-      author={faker.name.firstName()}
-      timeAgo="Today at 4:45PM"
-      avatar={faker.image.avatar()}
-      content={faker.random.words()}
-    />
-    <CommentDetail
-      author={faker.name.firstName()}
-      timeAgo="Today at 4:45PM"
-      avatar={faker.image.avatar()}
-      content={faker.random.words()}
-    />
+    <ApprovalCard>
+      <div>
+        <h4>Warning!</h4>
+        Are you sure you want to do this?
+      </div>
+    </ApprovalCard>
+
+    <ApprovalCard>
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today at 4:45PM"
+        avatar={faker.image.avatar()}
+        content={faker.random.words()}
+      />
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today at 4:45PM"
+        avatar={faker.image.avatar()}
+        content={faker.random.words()}
+      />
+    </ApprovalCard>
+    <ApprovalCard>
+      <CommentDetail
+        author={faker.name.firstName()}
+        timeAgo="Today at 4:45PM"
+        avatar={faker.image.avatar()}
+        content={faker.random.words()}
+      />
+    </ApprovalCard>
   </div>
 );
 
